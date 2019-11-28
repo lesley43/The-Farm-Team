efd3334caa5ba3fa9ed58eb49633e1c4e26002d5
Justin Johns <43861450+justinrjohns@users.noreply.github.com>
2019-11-27 22:43:58 -0500

    Delete fakeTypeData.js

--- src/data/fakeTypeData.js
+++
@@ -1,9 +0,0 @@
-export const types = [
-    { _id: "5b21ca3eeb7f6fbccd471814", name: "Fruit" },
-    { _id: "5b21ca3eeb7f6fbccd471816", name: "Herb" },
-    { _id: "5b21ca3eeb7f6fbccd471818", name: "Vegetable" }
-  ];
-  
-  export function getTypes() {
-    return types.filter(t => t);
-  }
